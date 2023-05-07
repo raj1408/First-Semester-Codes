dc40a68e1be55501dc3a2e246ede96cef7bffbc3
Raj Saini <101448363+raj1408@users.noreply.github.com>
2023-05-07 15:39:37 +0530

    Update Prime Number Print In a File.c

--- Prime Number Print In a File.c
+++ Prime Number Print In a File.c
@@ -47,7 +47,7 @@ void main()
     }
     else
     {
-        printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+        printf("Thanks for using our program.\nDeveloped by Er Raj Saini");
     }
     getch();
 
